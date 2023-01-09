aecce222c916835c83a1d6f96ba43db5afed77aa
aiseblaze <80523977+aiseblaze@users.noreply.github.com>
2023-01-09 12:23:56 +0100

    up

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     <NuxtWelcome />
+    <template lang="pug">
+  #app
+    router-view
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2C3E50;
+}
+</style>
   </div>
 </template>
